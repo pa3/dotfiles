c453ee3c4f9a973929fb43e38ed1cb8831a610b0
Fedor Kulikov <kulikov@alternativaplatform.com>
2016-12-02 19:10:57 +0500

    Changes emacs archive files location

--- init.el
+++ init.el
@@ -133,6 +133,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(backup-by-copying t)
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/backup"))))
+ '(css-indent-offset 2)
  '(custom-safe-themes
    (quote
     ("557c283f4f9d461f897b8cac5329f1f39fac785aa684b78949ff329c33f947ec" "c59857e3e950131e0c17c65711f1812d20a54b829115b7c522672ae6ba0864cc" "6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" default)))
