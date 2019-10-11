72455a56576694fb421fb3527b18a7f26ed2bc9c
Fedor Kulikov <f.kulikov@gmail.com>
2019-10-11 22:50:42 +0200

    emacs: disable ivy-mode fuzzy match

--- .emacs.d/load.d/ivy.el
+++ .emacs.d/load.d/ivy.el
@@ -1,14 +1,9 @@
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
-(setq ivy-re-builders-alist
-      '((t . ivy--regex-fuzzy)))
 
-;; enable this if you want `swiper' to use it
-;; (setq search-default-mode #'char-fold-to-regexp)
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
