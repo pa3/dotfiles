a739c3fef35841ff6cdab0fe3c982506f3cb194e
Fedor Kulikov <f.kulikov@gmail.com>
2019-10-12 00:01:51 +0200

    stumpwm: connect to emacs via swank server

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -1,9 +1,12 @@
-;; Font
-
-(ql:quickload "clx-truetype")
+;; font
+(ql:quickload :clx-truetype)
 (load-module "ttf-fonts")
 (set-font (make-instance
            'xft:font
            :family "Droid Sans Mono"
            :subfamily "Regular"
            :size 11))
+
+;; connect to emacs
+(ql:quickload :swank)
+(swank:create-server)
