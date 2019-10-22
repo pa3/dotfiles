e0c0736f8ce2c012ffe31d447cc54c53f9f87207
Fedor Kulikov <f.kulikov@gmail.com>
2019-10-23 01:04:14 +0200

    stumpwm: use DejaVu font (failed to use Hack)

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -1,14 +1,18 @@
-;; font
-(ql:quickload :clx-truetype)
-(load-module "ttf-fonts")
-(set-font (make-instance
-           'xft:font
-           :family "Droid Sans Mono"
-           :subfamily "Regular"
-           :size 11))
-
 ;; connect to emacs
 (ql:quickload :swank)
 (swank:create-server :port 4005
                      :style swank:*communication-style*
                      :dont-close t)
+
+(stumpwm:mode-line)
+
+;; font
+(ql:quickload :clx-truetype)
+(load-module "ttf-fonts")
+;; in case new font is added:
+;; (xft:cache-fonts)
+(set-font (make-instance
+           'xft:font
+           :family "DejaVu Sans Mono"
+           :subfamily "Book"
+           :size 13))
