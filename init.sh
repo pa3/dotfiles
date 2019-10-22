411cb098b0ead06d01b2dd3fc79e5c5770b3c34e
Fedor Kulikov <f.kulikov@gmail.com>
2019-10-23 00:19:33 +0200

    x11: initial commit of x11 configs

--- init.sh
+++ init.sh
@@ -3,3 +3,7 @@
 ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/.emacs.d ~/.emacs.d
 ln -s $PWD/.stumpwm.d ~/.stumpwm.d
+
+ln -s $PWD/.Xresources.d ~/.Xresources.d
+ln -s $PWD/.xbindkeysrc ~/.xbindkeysrc
+ln -s $PWD/.xinitrc ~/.xinitrc
